9510d0064e4a71e1550e3c7b101c2075445d18e9
Nikolas Alexsander <55640359+NKLzT@users.noreply.github.com>
2022-11-24 14:44:44 -0300

    Delete Cond.py

--- Cond.py
+++
@@ -1,9 +0,0 @@
-valor_passagem = 5
-valor_corrida = input('Qual o valor da corrida')
-
-if float(valor_corrida) <= valor_passagem * 5:
-    print('Pague a corrida')
-elif float(valor_corrida) <= valor_passagem * 6:
-    print("Aguarde um momento, o valor pode abaixar")
-else:
-    print('Pegue o ônibus')
